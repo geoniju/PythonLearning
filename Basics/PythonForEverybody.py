b87c57ecd2dca566fb6f2ea96e0d8de392c69891
Geo <geoniju@live.com>
2020-02-08 11:11:14 +0530

    made some progress in python for everybody

---
+++ Basics/PythonForEverybody.py
@@ -0,0 +1,14 @@
+# using try,except and open
+fname = input('Enter the file name')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+count = 0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print('Number of lines starting with Subject: ', count)
+
