1b72fe1fac0aae02ee79feef19629a23bb406d94
Geo <geoniju@live.com>
2020-02-25 16:42:33 +0530

    made some progress

---
+++ Basics/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "aCHvVXMEfrCV1oGSy1Od1fnqT",
+            "consumer_secret": "jVzdvFIhIITRXMYHLVvrd2bW3vIfUqvJT3g9aukPGajeyPbayD",
+            "token_key": "310386480-FM7zKkuMbxKwlMrvMlEq9wrBw17ctJwpecNVeexa",
+            "token_secret": "Alg8MyhHbcMfMnq2Gj7Kva36kk1e99snlnRfiL7i2vpXt"}
