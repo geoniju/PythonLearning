3a07968ab36149b428c0e72b76fe112beb2cc6b5
Geo <geoniju@live.com>
2020-03-02 22:08:21 +0530

    removed key

--- Basics/hidden.py
+++ Basics/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "aCHvVXMEfrCV1oGSy1Od1fnqT",
-            "consumer_secret": "jVzdvFIhIITRXMYHLVvrd2bW3vIfUqvJT3g9aukPGajeyPbayD",
-            "token_key": "310386480-FM7zKkuMbxKwlMrvMlEq9wrBw17ctJwpecNVeexa",
-            "token_secret": "Alg8MyhHbcMfMnq2Gj7Kva36kk1e99snlnRfiL7i2vpXt"}
+    return {"consumer_key": "",
+            "consumer_secret": "",
+            "token_key": "",
+            "token_secret": ""}
